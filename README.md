8a921abe8465ab17bb6ea5f681159a97d78765e0
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-04-08 13:11:24 +0000

    Use C++ & KDE Kirigami instead of Rust & Slint

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # Blitz Backup
-Backup Software With Compression, Deduplication And Encryption
-
-## About
-
-This template helps you get started developing a Rust application with Slint as toolkit
-for the user interface. It demonstrates the integration between the `.slint` UI markup and
-Rust code, how to trigger react to callbacks, get and set properties and use basic widgets.
+Backup Software Built on Restic
---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <KLocalizedContext>
+#include <KLocalizedString>
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+#include <QUrl>
+#include <QtQml>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    KLocalizedString::setApplicationDomain("helloworld");
+    QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
+    QCoreApplication::setOrganizationDomain(QStringLiteral("kde.org"));
+    QCoreApplication::setApplicationName(QStringLiteral("Hello World"));
+
+    if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE")) {
+        QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
+    }
+
+    QQmlApplicationEngine engine;
+
+    engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    if (engine.rootObjects().isEmpty()) {
+        return -1;
+    }
+
+    return app.exec();
+}
