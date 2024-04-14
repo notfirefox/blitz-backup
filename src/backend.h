3962c74e24eab6ca819254ce0eac00eecedf5aa7
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-04-14 16:05:30 +0000

    Add `loading` property to backend class

--- src/backend.h
+++ src/backend.h
@@ -4,15 +4,24 @@
 #include <QObject>
 #include <qobjectdefs.h>
 #include <qprocess.h>
+#include <qtmetamacros.h>
 
 class Backend : public QObject {
     Q_OBJECT
 
+    Q_PROPERTY(bool loading READ loading WRITE setLoading NOTIFY loadingChanged)
+
   private:
+    bool mLoading;
     QProcess *process;
 
   public:
     explicit Backend(QObject *parent = nullptr);
+
+    bool loading() const;
+    void setLoading(const bool loading);
+    Q_SIGNAL void loadingChanged();
+
     Q_INVOKABLE void backup();
     Q_INVOKABLE void refresh();
     Q_INVOKABLE void mount(const QString &);
